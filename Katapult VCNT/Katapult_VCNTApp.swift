e70253cde16b5efab3ad25d419cb362c57ddb0b5
Kleiber Perez <kleiber.perez@rappi.com>
2025-03-12 15:29:44 +0100

    Initial Commit

---
+++ Katapult VCNT/Katapult_VCNTApp.swift
@@ -0,0 +1,17 @@
+//
+//  Katapult_VCNTApp.swift
+//  Katapult VCNT
+//
+//  Created by Kleiber Perez on 12/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Katapult_VCNTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
